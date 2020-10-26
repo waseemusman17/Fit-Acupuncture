82f7e5a67b1a548146fc06a4630c417bbbbee723
waseemusman17 <waseeem.usman17@gmail.com>
2020-10-26 00:58:43 -0700

    Home Page Almost complete

---
+++ assets/js/custom.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    // Team Slider
+    $('.testimonials.owl-carousel').owlCarousel({
+            loop: true,
+            stagePadding: 140,
+            margin: 30,
+            nav: false,
+            autoplay: true,
+            dots: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 4
+                }
+            }
+        });
+});
