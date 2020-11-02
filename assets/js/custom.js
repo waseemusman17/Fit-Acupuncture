66324fb0e68ff3a2995e2c3aec95d16b85c8ddae
waseemusman17 <waseeem.usman17@gmail.com>
2020-11-02 15:17:54 +0500

    Home Page Full responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,26 +1,36 @@
 $(document).ready(function(){
     // Team Slider
     $('.testimonials.owl-carousel').owlCarousel({
-            loop: true,
-            stagePadding: 140,
-            margin: 30,
-            nav: false,
-            autoplay: true,
-            dots: true,
-            responsive: {
-                0: {
-                    items: 1,
-                    stagePadding:0
-                },
-                600: {
-                    items: 2
-                },
-                1000: {
-                    items: 3
-                },
-                1600: {
-                    items: 4
-                }
+        loop: true,
+        stagePadding: 140,
+        margin: 30,
+        nav: false,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding:0
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            },
+            1600: {
+                items: 4
             }
-        });
+        }
+    });
+
+
+
+    //Mobile NavBar
+    
+    $(".nav-toggler").click(function () {
+        $('.site-menu nav').slideToggle('menu-active');
+        $(this).toggleClass('nav-active');
+       
+    });
 });
